2730f8dfde6e1fa596f50193609fc7cbe31131ef
Andur Carr <carr.andur@gmail.com>
2017-01-31 16:58:43 -0800

    Add analytics query (taking a break)

--- app/models/shopper_applicant.rb
+++ app/models/shopper_applicant.rb
@@ -7,4 +7,24 @@ class ShopperApplicant < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :phone_number, presence: true
   validates :zip_code, presence: true
+
+  scope :applied_between, -> (start_date, end_date) {
+    where(
+      "shopper_applicants.created_at > ? AND shopper_applicants.created_at < ?",
+      start_date, end_date
+    )
+  }
+
+  def self.funnel_analytics(start_date, end_date)
+    joins(:funnels).
+    applied_between(start_date, end_date).
+    group("week_start").
+    select(
+      "date_trunc('week', shopper_applicants.created_at) AS week_start, " +
+      "COUNT(*) AS applied, " +
+      Funnel::STEPS.map do |step|
+        "SUM(CASE WHEN funnels.name = '#{step}' THEN 1 ELSE 0 END) as #{step}"
+      end.join(", ")
+    )
+  end
 end
