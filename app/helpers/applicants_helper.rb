74c3566876113315164e972b94a168f7bb5a9523
Andur Carr <carr.andur@gmail.com>
2017-02-02 16:39:13 -0800

    Add validation error messages

--- app/helpers/applicants_helper.rb
+++ app/helpers/applicants_helper.rb
@@ -2,4 +2,14 @@ module ApplicantsHelper
   def submit_message(applicant)
     applicant.persisted? ? "Update Application" : "Apply!"
   end
+
+  def inline_errors(errors, field_name)
+    content_tag(:span,
+      field_name.to_s.gsub("_", " ").capitalize +
+      " " +
+      errors[field_name].join(", ") +
+      ".",
+      class: "text-danger"
+    ) if errors[field_name].present?
+  end
 end
